11e1af7eb3e96beaf358a24029b4e9e699e9f01c
Tsukasa Nishiyama <tuka@trkg.jp>
2018-09-24 15:22:13 +0900

    :sparkles: add hatena bookmarks

---
+++ handlers/hatebu-total-bookmarks.ts
@@ -0,0 +1,35 @@
+import { Handler, Context, Callback } from "aws-lambda";
+import Axios from "axios";
+import * as moment from "moment";
+
+import Mackerel from "../lib/mackerel";
+
+export const Origin: string = 'http://api.b.st-hatena.com'
+export const Endpoint: string = '/entry.total_count'
+
+const env = {
+  BOOKMARK_URL: '',
+  MACKEREL_API_KEY: '',
+  MACKEREL_SERVICE_NAME: '',
+  MACKEREL_METRIC_NAME: '',
+  ...process.env
+}
+
+export const run: Handler =
+  async (event: string, context: Context, callback: Callback) => {
+
+    const total_bookmarks: Number = await Axios.request({
+      baseURL: Origin,
+      url: Endpoint,
+      params: { url: env.BOOKMARK_URL }
+    }).then(r => Number(r.data.total_bookmarks))
+
+    const payload = [{
+      name: env.MACKEREL_METRIC_NAME,
+      time: moment().unix(),
+      value: total_bookmarks
+    }]
+
+    const mackerel: Mackerel = new Mackerel(env.MACKEREL_API_KEY)
+    return await mackerel.postServiceMetric(payload, env.MACKEREL_SERVICE_NAME)
+  }
---
+++ test/hatebu-total-bookmarks.test.ts
@@ -0,0 +1,50 @@
+import { describe, before, it } from "mocha";
+import { assert } from "chai";
+import * as nock from "nock";
+import { Context, Callback } from "aws-lambda";
+import * as MockContext from "aws-lambda-mock-context";
+
+const DUMMY_SITE_URL: string = 'http://example.com/'
+process.env.BOOKMARK_URL = DUMMY_SITE_URL
+process.env.MACKEREL_API_KEY = "DUMMY_API_KEY"
+process.env.MACKEREL_SERVICE_NAME = "hatebu_test"
+process.env.MACKEREL_METRIC_NAME = "はてなブックマーク数推移.total-bookmarks"
+
+import { run, Origin as Hatebu_Origin, Endpoint as Hatebu_Endpoint } from "../handlers/hatebu-total-bookmarks";
+import { Origin as Mackerel_Origin } from "../lib/mackerel";
+
+describe('Hatena bookmark Handler', () => {
+
+  // mock hatena bookmark api
+  nock(Hatebu_Origin)
+    .get(Hatebu_Endpoint)
+      .query({
+        url: DUMMY_SITE_URL
+      })
+      .reply(200, {
+        url: DUMMY_SITE_URL,
+        total_bookmarks: 1009
+      })
+
+  // mock mackerel api
+  nock(Mackerel_Origin)
+    .filteringRequestBody(body => {
+      let a = eval(body)[0]
+      delete a.time
+      return a
+    })
+    .post(`/api/v0/services/${process.env.MACKEREL_SERVICE_NAME}/tsdb`, {
+      name: process.env.MACKEREL_METRIC_NAME,
+      value: 1009
+    })
+      .reply(200, { success: true })
+
+  it('', async () => {
+    const context: Context = MockContext(),
+          callback: Callback = () => {}
+
+    const res = await run('test', context, callback)
+    assert.strictEqual(res.status, 200)
+    assert.strictEqual(res.data.success, true)
+  })
+})
